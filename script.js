2dce764c7bde3c245fb9b69afb6439cca1e9f4a9
Juliana Araujo <julianaraujo555@outlook.com>
2021-11-28 22:32:16 -0300

    update scroll

--- script.js
+++ script.js
@@ -60,3 +60,28 @@ function initScrollSuave(){
 initScrollSuave();
 
 
+function initAnimacaoScroll(){
+  const sections = document.querySelectorAll('.js-scroll');
+
+  if(sections.length){
+    const windowMetade = window.innerHeight * 0.6;
+
+    function animaScroll(){
+      sections.forEach((section) => {
+        const sectionTop = section.getBoundingClientRect().top - windowMetade;
+        console.log(sectionTop)
+        if(sectionTop < 0){
+          section.classList.add('ativo')
+        }
+      })
+    }
+
+    animaScroll();
+
+    window.addEventListener('scroll', animaScroll);
+  }
+}
+
+initAnimacaoScroll();
+
+
